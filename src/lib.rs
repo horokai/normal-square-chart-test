c22d8a4ce39b9dd0621513cc46f8c68a295ad520
horokai <horokai@outlook.jp>
2019-11-11 12:15:31 +0900

    Universe Test

--- src/lib.rs
+++ src/lib.rs
@@ -5,3 +5,68 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
+pub struct Universe {
+    length: u32,
+    count: u32,
+    view_info: Vec<u8>,
+}
+
+impl Universe {
+    pub fn countup(&mut self) {
+        self.count += 1;
+    }
+
+    pub fn new(length: u32) -> Universe {
+        let view_info = vec![0u8; (length * length) as usize];
+        Universe {
+            length,
+            count: 0u32,
+            view_info,
+        }
+    }
+
+    pub fn get_length(&self) -> u32 {
+        self.length
+    }
+
+    pub fn get_count(&self) -> u32 {
+        self.count
+    }
+
+    pub fn get_view_info(&self) -> *const u8 {
+        self.view_info.as_ptr()
+    }
+
+    pub fn reset(&mut self, length: u32) {
+        self.length = length;
+        self.count = 0u32;
+        let size = self.view_info.len();
+        for i in 0..size {
+            self.view_info[i as usize] = 0;
+        }
+    }
+
+    pub fn reverse(&mut self) {
+        let size = self.view_info.len();
+        for i in 0..size {
+            if self.view_info[i as usize] == 0 {
+                self.view_info[i as usize] = 1;
+            } else {
+                self.view_info[i as usize] = 0;
+            }
+        }
+    }
+
+    pub fn tick(&mut self) {
+        let count = self.count as usize;
+        if self.view_info[count as usize] == 0 {
+            self.view_info[count as usize] = 1;
+        } else {
+            self.view_info[count as usize] = 0;
+        }
+        let size = self.view_info.len();
+        self.count = (self.count + 1) % size as u32;
+    }
+}
+
